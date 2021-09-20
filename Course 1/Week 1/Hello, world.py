dcf87676ed66dd334bdf9b81d2a5284ef4b1bb70
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-09-19 23:46:40 -0400

    Update Hello, world.py

--- Course 1/Week 1/Hello, world.py
+++ Course 1/Week 1/Hello, world.py
@@ -11,7 +11,7 @@ model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 #sgd: Stochastic Gradient Descent
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-# y = 2x - 1
+# f(x) = 2x - 1
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
