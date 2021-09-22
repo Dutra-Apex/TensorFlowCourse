d5d7ff04a0db18100cdacdbe37fcca8b866dc976
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-09-21 23:32:43 -0400

    Update Fashion_Mnist.py

--- Course 1/Week 2/Fashion_Mnist.py
+++ Course 1/Week 2/Fashion_Mnist.py
@@ -6,9 +6,14 @@ import keras
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-#Builds a model with 3 layers,
+#Builds a model with 3 layers:
+
 #The 1st layer takes 28 x 28 inputs, with is the number of pixels in each image
 #Flatten is used to turn the input into a linear array
+
+#The 2nd layer is a Dense layer with 128 neurons
+
+#The 3rd layer outputs 10 neurons, as there are 10 different types of clothing
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
