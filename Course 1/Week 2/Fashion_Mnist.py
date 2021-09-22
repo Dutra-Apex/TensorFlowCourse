2de36acd87a68d6f3da496ca7b45b32121ada23e
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-09-21 23:30:34 -0400

    Create Fashion_Mnist.py

---
+++ Course 1/Week 2/Fashion_Mnist.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import keras
+
+#Import a lis of images of clothing from fashion_mnist
+#The images are 28x28 and have already been greyscaled
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+#Builds a model with 3 layers,
+#The 1st layer takes 28 x 28 inputs, with is the number of pixels in each image
+#Flatten is used to turn the input into a linear array
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation = tf.nn.relu),
+    keras.layers.Dense(10, activation = tf.nn.softmax)
+])
