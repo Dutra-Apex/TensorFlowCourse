d10e6433f569cc5759662e7e6669ad47d087c739
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-10-12 00:22:55 -0400

    Update Callback_class.py

--- Course 1/Week 2/Callback_class.py
+++ Course 1/Week 2/Callback_class.py
@@ -3,6 +3,7 @@ x = 0.1
 
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
+        #logs.get('accuracy') >= 0.6
         if(logs.get('loss')< x):
             print("\nLoss is low, training stops")
             self.model.stop_training = True
